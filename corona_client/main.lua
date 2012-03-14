d9446cc2ebc6dceea3e9edd05ce634e9607b4077
Kevin Harris <tokevinharris@gmail.com>
2012-03-14 00:10:17 -0500

    Adding test code for creating games.

--- corona_client/main.lua
+++ corona_client/main.lua
@@ -1,7 +1,7 @@
 local json = require "json"
 
-local serverAddress = "http://floating-sunset-9026.herokuapp.com"
---local serverAddress = "http://0.0.0.0:3000"
+--local serverAddress = "http://floating-sunset-9026.herokuapp.com"
+local serverAddress = "http://0.0.0.0:3000"
 
 local myText = display.newText("(Waiting for response)", 0, 0, native.systemFont, 16)
 myText.x = display.contentCenterX
@@ -31,7 +31,6 @@ local function destroySession( event )
 	else
 		myText.text = "See Corona Terminal for response"
 		print( event.response )
-		
 		print("")
 		
 		--network.request( serverAddress .. "/users/show.json?id=50", "GET", showUser2 )
@@ -52,6 +51,11 @@ local function getChats( event )
 		print("")
 
 		local listOfChats = json.decode( event.response )
+		
+		if #listOfChats == 0 then
+			print( "No chats!" )
+			return
+		end
 		
 		for i = 1, #listOfChats do
 			print( "id = " .. listOfChats[i]["id"] )
@@ -79,6 +83,11 @@ local function getMoves( event )
 		print("")
 
 		local listOfMoves = json.decode( event.response )
+
+		if #listOfMoves == 0 then
+			print( "No moves!" )
+			return
+		end
 		
 		for i = 1, #listOfMoves do
 			print( "id = " .. listOfMoves[i]["id"] )
@@ -92,33 +101,6 @@ local function getMoves( event )
 
 end
 
-local function getGames( event )
-
-	print( "------------------ getGames ------------------" ) 
-
-	if ( event.isError ) then
-		myText.text = "Network error!"
-	else
-		myText.text = "See Corona Terminal for response"
-		print( event.response )
-
-		print("")
-
-		local listOfGames = json.decode( event.response )
-		
-		for i = 1, #listOfGames do
-			print( "id = " .. listOfGames[i]["id"] )
-			print( "user_id_1 = " .. listOfGames[i]["user_id_1"] )
-			print( "user_id_2 = " .. listOfGames[i]["user_id_2"] )
-			print( "num_moves = " .. listOfGames[i]["num_moves"] )
-			print( "num_chats = " .. listOfGames[i]["num_chats"] )
-			print("")
-		end
-
-		network.request( serverAddress .. "/users/moves.json?id=" .. listOfGames[1]["id"], "GET", getMoves )
-	end
-
-end
 --[[
 local function showUser( event )
 
@@ -146,8 +128,30 @@ local function showUser( event )
 end
 --]]
 
+local function createChat( event )
 
+	print( "------------------ createChat ------------------" ) 
+	
+	if ( event.isError ) then
+		myText.text = "Network error!"
+	else
+		myText.text = "See Corona Terminal for response"
+		print( event.response )
+		
+		print("")
+		
+		local chatInfo = json.decode( event.response )
+		--print( "id = " .. chatInfo["id"] )
+		print( "user_id = " .. chatInfo["user_id"] )
+		print( "game_id = " .. chatInfo["game_id"] )
+		print( "message = " .. chatInfo["message"] )
+		print("")
+				
+		network.request( serverAddress .. "/sessions/destroy.json", "GET", destroySession )
+		--network.request( serverAddress .. "/users/moves.json?id=" .. chatInfo["user_id"], "GET", getMoves )
+	end
 
+end
 
 local function createMove( event )
 
@@ -160,13 +164,81 @@ local function createMove( event )
 		print( event.response )
 		
 		print("")
+		
+		local moveInfo = json.decode( event.response )
+		--print( "id = " .. moveInfo["id"] )
+		print( "user_id = " .. moveInfo["user_id"] )
+		print( "game_id = " .. moveInfo["game_id"] )
+		print( "data = " .. moveInfo["data"] )
+		print("")
+				
+		--network.request( serverAddress .. "/sessions/destroy.json", "GET", destroySession )
+		network.request( serverAddress .. "/chats/create.json?game_id=" .. moveInfo["game_id"] .. "&message=This%20is%20a%20chat", "GET", createChat )
 	end
 
 end
 
+local function getGames( event )
 
+	print( "------------------ getGames ------------------" ) 
 
+	if ( event.isError ) then
+		myText.text = "Network error!"
+	else
+		myText.text = "See Corona Terminal for response"
+		print( event.response )
 
+		print("")
+
+		local listOfGames = json.decode( event.response )
+		
+		if #listOfGames == 0 then
+			print( "No games!" )
+			return
+		end
+		
+		for i = 1, #listOfGames do
+			print( "id = " .. listOfGames[i]["id"] )
+			print( "user_id_1 = " .. listOfGames[i]["user_id_1"] )
+			print( "user_id_2 = " .. listOfGames[i]["user_id_2"] )
+			print( "num_moves = " .. listOfGames[i]["num_moves"] )
+			print( "num_chats = " .. listOfGames[i]["num_chats"] )
+			print("")
+		end
+
+		print("")
+		
+		network.request( serverAddress .. "/users/moves.json?id=" .. listOfGames[2]["id"], "GET", getMoves )
+		--network.request( serverAddress .. "/moves/create.json?game_id=" .. listOfGames[1]["id"] .. "&data=JSON%20data%20with%20spaces", "GET", createMove )
+	end
+
+end
+
+local function createGame( event )
+
+	print( "------------------ createGame ------------------" ) 
+
+	if ( event.isError ) then
+		myText.text = "Network error!"
+	else
+		myText.text = "See Corona Terminal for response"
+		print( event.response )
+
+		print("")
+
+		local game = json.decode( event.response )
+		
+		print( "id = " .. game["id"] )
+		print( "user_id_1 = " .. game["user_id_1"] )
+		print( "user_id_2 = " .. game["user_id_2"] )
+		print( "num_moves = " .. game["num_moves"] )
+		print( "num_chats = " .. game["num_chats"] )
+		print("")
+
+		network.request( serverAddress .. "/sessions/destroy.json", "GET", destroySession )
+	end
+
+end
 
 local function createSession( event )
 
@@ -184,15 +256,22 @@ local function createSession( event )
 		print( "id = " .. userInfo["id"] )
 		print( "name = " .. userInfo["name"] )
 		print( "email = " .. userInfo["email"] )
+		print("")
 		
-		-- OK, we created a session, now lets try to show a user.
 		--network.request( serverAddress .. "/users/show.json?id=" .. userInfo["id"], "GET", showUser )
-		
 		network.request( serverAddress .. "/users/games.json?id=" .. userInfo["id"], "GET", getGames )
+		--network.request( serverAddress .. "/games/create.json?opponent_id=55", "GET", createGame )
+
+	end
+
+end
+
+print( "\n\n\n" )
+
+network.request( serverAddress .. "/sessions/create.json?email=test1@test.com&password=password1", "GET", createSession )
 
 
---network.request( serverAddress .. "/moves/create.json?game_id=2&data=JSON%20data%20with%20spaces", "GET", createMove )
-
+-- http://developer.anscamobile.com/node/5305
 
 --[[
 postData = "game_id=2&data=JSONdata2"
@@ -202,44 +281,6 @@ params.body = postData
 
 network.request( serverAddress .. "/moves/create.json", "POST", createMove, params )
 --]]
-	end
-
-end
-
-print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-
-network.request( serverAddress .. "/sessions/create.json?email=test1@test.com&password=password1", "GET", createSession )
-
-
-
-
-
-
-
-
--- local function createSession( event )
--- 
--- 	if ( event.isError ) then
--- 		myText.text = "Network error!"
--- 	else
--- 		myText.text = "See Corona Terminal for response"
--- 		
--- 		print( event.response )
--- 		
--- 		--local listOfUsers = json.decode(event.response)
--- 		--for i = 1, #listOfUsers do
--- 		--	print( listOfUsers[i]["name"] )
--- 		--	print( listOfUsers[i]["email"] )
--- 		--end
--- 
--- 	end
--- 
--- end
-
---network.request( "http://floating-sunset-9026.herokuapp.com/users/list.json", "GET", networkListener )
-
-
--- http://developer.anscamobile.com/node/5305
 
 --[[
 postData = "name=Mr Test3&email=test3@test.com&password=testtest&password_confirmation=testtest22"
